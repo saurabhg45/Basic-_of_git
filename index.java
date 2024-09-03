f18e2d48e2f9e99d4621f6531ac4c3366258af44
Saurabh Gayke <gaykesaurabh45@gmail.com>
2024-09-03 16:15:21 +0530

    1st commet

---
+++ index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const age = 24;
+const name = "Saurabh Gayke";
+const dob = "10/08/2001";
+
+console.log(name);
