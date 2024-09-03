c8056303479535852b656a42a0f3eba78ccc0188
Saurabh Gayke <gaykesaurabh45@gmail.com>
2024-09-03 17:22:20 +0530

    Even\Odd

--- index.java
+++ index.java
@@ -3,11 +3,16 @@ import java.util.*;
 public class index {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
-        System.out.println(a);
+        if (a % 2 == 0) {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
         sc.close();
     }
 }
